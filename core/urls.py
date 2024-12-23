61a18ba5d59ce9805a7186471aa38340058bb533
techie-guy92 <soheil.dalirii@gmail.com>
2024-12-23 09:27:04 +0330

    main url settings has  been done

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include("users.urls")),
+    path('', include("main.urls")),
+    
+    # third parties
+    path("api-auth/", include("rest_framework.urls")),
+    path("api-schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api-documents", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui")
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_title = "Easy Buy"
+admin.site.index_title = "Easy Buy"
+admin.site.site_header = "Easy Buy Administration"
---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+
+
+
+urlpatterns = []
+
+
+
+urlpatterns += router.urls
