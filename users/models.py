e047dabb314bcd291f221a4bc817cae52853207a
techie-guy92 <soheil.dalirii@gmail.com>
2024-12-29 14:31:54 +0330

    language changed to english

--- users/models.py
+++ users/models.py
@@ -65,22 +65,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     joined_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Joined At")
     updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name="Updated At")
 
-    # groups = models.ManyToManyField(
-    #     "auth.Group",
-    #     related_name="customuser_set",  
-    #     blank=True,
-    #     help_text="The groups this user belongs to.",
-    #     verbose_name="groups",
-    # )
-    
-    # user_permissions = models.ManyToManyField(
-    #     "auth.Permission",
-    #     related_name="customuser_permissions_set",  
-    #     blank=True,
-    #     help_text="Specific permissions for this user.",
-    #     verbose_name="user permissions",
-    # )
-
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
 
