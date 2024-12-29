741678fe9aabb97d19549c4946c0e3b344b26f24
techie-guy92 <soheil.dalirii@gmail.com>
2024-12-29 13:11:02 +0330

    all models have been defined

--- main/models.py
+++ main/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from os import path
+from uuid import uuid4
+from users.models import *
 
-# Create your models here.
+
+#======================================= Needed Method ======================================================
+
+def upload_to(instance, filename):
+    file_name, ext = path.splitext(filename)
+    new_filename = f"{uuid4() } {ext}"
+    try:
+        model = getattr(instance, "category", None) or getattr(instance, "product", None) or getattr(instance, "gallery", "deafult_gallery") 
+        if isinstance(instance, Category):
+            return f"images/categories/{model}/{new_filename}"
+        elif isinstance(instance, Product):
+            return f"images/products/{model}/{new_filename}"
+        else:
+            return f"images/galleries/{model}/{new_filename}"
+    except AttributeError:
+        return f"images/others/{new_filename}"
+    
+
+#======================================= Category Model =====================================================
+
+class Category(models.Model):
+    category =models.CharField(max_length=30, verbose_name="Category")
+    parent = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="Category_parent", blank=True, null=True, verbose_name="Parent")
+    slug = models.SlugField(unique=True, verbose_name="Slug")
+    description = models.TextField(blank=True, null=True, verbose_name="Description")
+    image= models.ImageField(upload_to=upload_to, blank=True, null=True, verbose_name="Image")
+    
+    def __str__(self):
+        return self.category
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        indexes = [
+            models.Index(fields=["category"]),
+            models.Index(fields=["parent"]),
+            models.Index(fields=["slug"]),
+        ]
+    
+
+#======================================= Ptoduct Model ======================================================
+
+class Product(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="Product_user", verbose_name="User")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="Product_category", verbose_name="Category")
+    product = models.CharField(max_length=100, verbose_name="Product")
+    slug = models.SlugField(unique=True, verbose_name="Slug")
+    price = models.PositiveIntegerField(default=0, verbose_name="Price")
+    description = models.TextField(blank=True, null=True, verbose_name="Description")
+    is_active = models.BooleanField(default=False, verbose_name="Being Active")
+    willing_exchange = models.BooleanField(default=False, verbose_name="Willing Exchange")
+    image = models.ImageField(upload_to=upload_to, blank=True, null=True, verbose_name="Image")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name="Updated At")
+    
+    def __str__(self):
+        return self.product
+    
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        indexes = [
+            models.Index(fields=["product"]),
+            models.Index(fields=["category"]),
+            models.Index(fields=["slug"]),
+            models.Index(fields=["price"]),
+            models.Index(fields=["is_active"]),
+            models.Index(fields=["willing_exchange"]),
+        ]
+
+
+#======================================= Gallery Model ======================================================
+
+class Gallery(models.Model):
+    gallery = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="Gallery_gallery", verbose_name="Product")
+    image = models.ImageField(upload_to=upload_to, blank=True, null=True, verbose_name="Image")
+    
+    def __str__(self):
+        return self.gallery
+    
+    class Meta:
+        verbose_name = "Gallery"
+        verbose_name_plural = "Galleries"
+
+
+#============================================================================================================
--- users/models.py
+++ users/models.py
@@ -1,3 +1,109 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
+from os import path
+from uuid import uuid4
 
-# Create your models here.
+
+#======================================= Needed Method ================================================
+
+def upload_to(instance, filename):
+    file_name, ext = path.splitext(filename)
+    new_filename = f"{uuid4()}{ext}"
+    user = instance.user
+    full_name = user.get_full_name().replace(" ", "")
+    return f"images/users/{full_name}/{new_filename}"
+
+
+#====================================== CustomUserManager Model =======================================
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, first_name, last_name, email, password=None):
+        if not email:
+            raise ValueError("وارد کردن ایمیل ضروری است.")
+        
+        user = self.model(
+            username = username,
+            first_name = first_name.capitalize(),
+            last_name = last_name.capitalize(),
+            email = self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.user_type = "user"
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, first_name, last_name, email, password=None):
+        user = self.create_user(
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = password,
+        )
+        user.is_active = True
+        user.is_admin = True
+        user.is_superuser = True
+        user.user_type = "backend"
+        user.save(using=self._db)
+        return user
+
+
+#======================================= CustomUser Model ===============================================
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE = [("backend", "BackEnd"), ("frotend", "FrontEnd"), ("admin", "Admin"), ("premium", "Premium"), ("user", "User")]
+    username = models.CharField(max_length=30, unique=True, verbose_name="Username")
+    first_name = models.CharField(max_length=30, verbose_name="First Nmae")
+    last_name = models.CharField(max_length=30, verbose_name="Last Nmae")
+    email = models.EmailField(max_length=100, unique=True, verbose_name="Email")
+    user_type = models.CharField(max_length=30, choices=USER_TYPE, default="user", verbose_name="User Type")
+    is_active = models.BooleanField(default=False, verbose_name="Being Active")
+    is_premium = models.BooleanField(default=False, verbose_name="Being Premium")
+    is_admin = models.BooleanField(default=False, verbose_name="Being Admin")
+    is_superuser = models.BooleanField(default=False, verbose_name="Being Superuser")
+    joined_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Joined At")
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name="Updated At")
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name", "email"]
+    
+    objects = CustomUserManager()
+    
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+
+
+#==================================== UserProfile Model =================================================
+
+class UserProfile(models.Model):
+    GENDER = [("male", "آقا"), ("female", "خانوم"), ("other", "نمی گویم")]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="UserProfile_user", verbose_name="User")
+    phone = models.CharField(max_length=12, unique=True, verbose_name="Phone Number")
+    gender = models.CharField(max_length=30, choices=GENDER, default="other", verbose_name="Gender")
+    address = models.TextField(verbose_name="Address")   
+    bio = models.TextField(blank=True, null=True, verbose_name="Bio")
+    profile_picture = models.ImageField(upload_to=upload_to, blank=True, null=True, verbose_name="Profile Picture")
+
+
+#==================================== PremiumSubscription Model =========================================
+
+class PremiumSubscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,  on_delete=models.CASCADE, related_name="PremiumSubscription_user", verbose_name="User")
+    start_date = models.DateTimeField(verbose_name="Start Date")
+    end_date = models.DateTimeField(verbose_name="End Date")
+    status = models.BooleanField(default=False, verbose_name="Status")
+    
+
+#========================================================================================================
