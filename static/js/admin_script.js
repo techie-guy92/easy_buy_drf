fa6bcb2bf629c01a948f5c1bf548498944a68345
techie-guy92 <soheil.dalirii@gmail.com>
2025-01-13 17:27:16 +0330

    made some changes in js code

--- static/js/admin_script.js
+++ static/js/admin_script.js
@@ -1,15 +1,38 @@
+// ======================================== first approach ========================================== 
 
-document.addEventListener("DOMContentLoaded", function () {
-    let firstNameInput = document.getElementById("id_first_name");
-    let lastNameInput = document.getElementById("id_last_name");
-    let usernameInput = document.getElementById("id_username");
+(function ($) {
+    $(document).ready(function () {
+        let firstNameInput = $("#id_first_name");
+        let lastNameInput = $("#id_last_name");
+        let usernameInput = $("#id_username");
 
-    function updateUsername() {
-        let firstName = firstNameInput.value.toLowerCase().trim();
-        let lastName = lastNameInput.value.toLowerCase().trim();
-        usernameInput.value = firstName && lastName ? `${firstName}_${lastName}` : "";
-    }
+        function updateUsername() {
+            let firstName = firstNameInput.val().toLowerCase().trim();
+            let lastName = lastNameInput.val().toLowerCase().trim();
+            usernameInput.val(`${firstName}_${lastName}`);
+        }
+        firstNameInput.on("input", updateUsername);
+        lastNameInput.on("input", updateUsername);
+    });
+})(django.jQuery);
 
-    firstNameInput.addEventListener("input", updateUsername);
-    lastNameInput.addEventListener("input", updateUsername);
-});
+
+// ======================================== second approach ==========================================
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     let firstNameInput = document.getElementById("id_first_name");
+//     let lastNameInput = document.getElementById("id_last_name");
+//     let usernameInput = document.getElementById("id_username");
+
+//     function updateUsername() {
+//         let firstName = firstNameInput.value.toLowerCase().trim();
+//         let lastName = lastNameInput.value.toLowerCase().trim();
+//         usernameInput.value = firstName && lastName ? `${firstName}_${lastName}` : "";
+//     }
+
+//     firstNameInput.addEventListener("input", updateUsername);
+//     lastNameInput.addEventListener("input", updateUsername);
+// });
+
+
+// ====================================================================================================
