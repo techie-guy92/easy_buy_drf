43c6e6e75d609f14fdcca79c2458e6465a27448d
techie-guy92 <soheil.dalirii@gmail.com>
2025-01-27 19:55:09 +0330

    fixed a little but for celery

--- celery_config.py
+++ celery_config.py
@@ -3,14 +3,14 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easy_buy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = Celery('easy_buy')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
+#   should have a 'CELERY_' prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
