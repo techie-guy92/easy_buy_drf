a608687262a60cf1c89ab18b747518df926006f3
techie-guy92 <soheil.dalirii@gmail.com>
2025-01-28 11:20:42 +0330

    modules for testings are ready

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import django
+import os
+django.setup()
+os.environ["DJANGO_SETTINGS_MODULE"] = "core.settings"
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from django.urls import resolve, reverse
+from model_bakery.baker import make
+import pytz
+from .models import *
+from .urls import *
+from .serializers import *
+from .views import *
+
+
+# Run tests using: python manage.py test
+# Run tests using: python manage.py test app_name.tests_module.TestClass
+
+#======================================== Product Add Test =========================================
+
+#======================================== Product Add Test =========================================
+
+#======================================== Product Add Test =========================================
+
+#===================================================================================================
