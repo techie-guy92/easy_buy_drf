dd4a2c02ee9b39400aafab7bace460a0ed3b7754
techie-guy92 <soheil.dalirii@gmail.com>
2025-01-31 08:21:23 +0330

    two tests were defined

--- main/tests.py
+++ main/tests.py
@@ -20,8 +20,6 @@ from .views import *
 
 #======================================== Needed chip ============================================
 
-client = APIClient()
-
 user_data_1 = {
     "username": "techie-guy92",
     "first_name": "Soheil",
@@ -63,6 +61,7 @@ product_data_1 = {
 
 product_data_2 = {
     "product": "پلی استیشن 5",
+    "slug": "playstation-5",
     "price": 1000,
     "willing_exchange": False
 }
@@ -73,6 +72,7 @@ product_data_2 = {
 class ProductAddTest(APITestCase):
     
     def setUp(self):
+        self.client = APIClient()
         self.url = reverse("product-add")
         
         self.user_1 = CustomUser.objects.create(
@@ -119,12 +119,14 @@ class ProductAddTest(APITestCase):
         self.assertEqual(serializer_data["product"], product_data_1["product"])
     
     def test_product_add_view_authenticated(self):
-        client.force_authenticate(user=self.user_1)
+        self.client.force_authenticate(user=self.user_1)
         product_data_2["category"] = self.category_2.id
-        response = client.post(self.url, product_data_2, format="json")
+        response = self.client.post(self.url, product_data_2, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["message"], "محصول شما ثبت شد، و پس از تایید کارشناسان درج خواهد شد.")
         
+    # If we use `client` without `self` (define clinet out of class), we might accidentally use a `client` that has already been authenticated in previous tests or has some other configuration, 
+	# leading to unexpected behavior like a `400 Bad Request` instead of `401 Unauthorized`.
     def test_product_add_view_unauthenticated(self):
         response = self.client.post(self.url, product_data_2, format="json")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -139,16 +141,69 @@ class ProductAddTest(APITestCase):
 class ProductDisplayTest(APITestCase):
     
     def setUp(self):
+        self.client = APIClient()
         self.url = reverse("products-list")
         
+        self.user_1 = CustomUser.objects.create(
+            username=user_data_1["username"],
+            first_name=user_data_1["first_name"],
+            last_name=user_data_1["last_name"],
+            email=user_data_1["email"]
+        )
+        self.user_1.is_active = user_data_1["is_active"]
+        self.user_1.set_password(user_data_1["password"])
+        self.user_1.save()
+        
+        self.user_2 = CustomUser.objects.create(
+            username=user_data_2["username"],
+            first_name=user_data_2["first_name"],
+            last_name=user_data_2["last_name"],
+            email=user_data_2["email"]
+        )
+        self.user_2.is_active = user_data_2["is_active"]
+        self.user_2.is_admin = user_data_2["is_admin"]
+        self.user_2.is_superuser = user_data_2["is_superuser"]
+        self.user_2.set_password(user_data_2["password"])
+        self.user_2.save()
+        
+        self.category_1 = Category.objects.create(category=category_data_1["category"], slug=category_data_1["slug"])
+        self.category_2 = Category.objects.create(category=category_data_2["category"], slug=category_data_2["slug"])
+        
+        self.product_1 = Product.objects.create(
+            user=self.user_1,
+            category=self.category_1,
+            product=product_data_1["product"],
+            slug=product_data_1["slug"],
+            price=product_data_1["price"]
+        )
+        self.product_1.is_active = True
+        self.product_1.save()
+        
+        self.product_2 = Product.objects.create(
+            user=self.user_2,
+            category=self.category_2,
+            product=product_data_2["product"],
+            slug=product_data_2["slug"],
+            price=product_data_2["price"]
+        )
+        self.product_2.is_active = True
+        self.product_2.save()
+        
     def test_product_display_view(self):
-        pass 
+        self.client.force_authenticate(user=self.user_1)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 2)
     
     def test_product_display_slug_view(self):
-        pass 
-       
+        self.client.force_authenticate(user=self.user_1)
+        url = reverse("products-detail", kwargs={"slug": self.product_1.slug})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["product"], self.product_1.product)
+        
     def test_product_display_url(self):
-        view = resolve("/product/")
+        view = resolve("/products/")
         self.assertEqual(view.func.cls, ProductDisplayViewSet)
         
 
@@ -157,7 +212,53 @@ class ProductDisplayTest(APITestCase):
 class ProductDetailTest(APITestCase):
     
     def setUp(self):
+        self.client = APIClient()
         self.url = reverse("product")
+        
+        self.user_1 = CustomUser.objects.create(
+            username=user_data_1["username"],
+            first_name=user_data_1["first_name"],
+            last_name=user_data_1["last_name"],
+            email=user_data_1["email"]
+        )
+        self.user_1.is_active = user_data_1["is_active"]
+        self.user_1.set_password(user_data_1["password"])
+        self.user_1.save()
+        
+        self.user_2 = CustomUser.objects.create(
+            username=user_data_2["username"],
+            first_name=user_data_2["first_name"],
+            last_name=user_data_2["last_name"],
+            email=user_data_2["email"]
+        )
+        self.user_2.is_active = user_data_2["is_active"]
+        self.user_2.is_admin = user_data_2["is_admin"]
+        self.user_2.is_superuser = user_data_2["is_superuser"]
+        self.user_2.set_password(user_data_2["password"])
+        self.user_2.save()
+        
+        self.category_1 = Category.objects.create(category=category_data_1["category"], slug=category_data_1["slug"])
+        self.category_2 = Category.objects.create(category=category_data_2["category"], slug=category_data_2["slug"])
+        
+        self.product_1 = Product.objects.create(
+            user=self.user_1,
+            category=self.category_1,
+            product=product_data_1["product"],
+            slug=product_data_1["slug"],
+            price=product_data_1["price"]
+        )
+        self.product_1.is_active = True
+        self.product_1.save()
+        
+        self.product_2 = Product.objects.create(
+            user=self.user_2,
+            category=self.category_2,
+            product=product_data_2["product"],
+            slug=product_data_2["slug"],
+            price=product_data_2["price"]
+        )
+        self.product_2.is_active = True
+        self.product_2.save()
     
     def test_product_detail_serializer(self):
         pass 
